76545062fb8a176f9f354f4d6534e12f28069638
Thanh Vũ <vuphamngocthanh@gmail.com>
2022-08-10 14:22:05 +0700

    tinh dien tich hinh chu nhat

---
+++ bien.js
@@ -0,0 +1,6 @@
+var inputWidth = Number(prompt("nhap vao chieu dai: "));
+var inputHight = Number(prompt("nhap vao chieu rong: "));
+
+var dientich = inputHight * inputWidth;
+
+document.write("Dien tich hinh chu nhat la : " + dientich);
